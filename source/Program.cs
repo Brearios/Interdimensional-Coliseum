101b69d8ca4283f380b618ca3ee462a843e9a7bc
Aaron Olson <presin16@gmail.com>
2019-09-23 21:19:25 -0500

    Added Fighter Class

--- source/Program.cs
+++ source/Program.cs
@@ -5,113 +5,104 @@ namespace InterdimensionalColiseum
 
     class Program
     {
-        static void Main(string[] args)
+    static void Main(string[] args)
         {
-            string heroName;
-            double heroMaxHealth;
-            int heroDamage;
-            int heroAtkSpd;
-            int heroAtkRdy = 0;
-            int heroMissChance = 3;
+
+            Console.WriteLine("Welcome to the Interdimensional Coliseum!");            
+            Console.WriteLine("Enter the name of your hero.");
+            string name = Console.ReadLine();
+            Console.WriteLine("How much health does " + name + " have?");
+            int maxHealth = Convert.ToInt32(Console.ReadLine());
+            int currHealth = maxHealth;
+            Console.WriteLine("How much damage does " + name + " do per attack?");
+            int damage = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("With 1 being very slow, and 10 being very fast, how fast does " + name + " attack?");
+            int atkSpd = Convert.ToInt32(Console.ReadLine());
+            
+            int atkRdy = 0;
+            int missChance = 3;
             int critRate = 10;
             int dodgeChance = 10;
-           
-            string enemyName;
-            double enemyMaxHealth;
-            int enemyDamage;
-            int enemyAtkSpd;
-            int enemyAtkRdy = 0;
-            int enemyMissChance = 3;
-            
-            // Welcome
-            Console.WriteLine("Welcome to the Interdimensional Coliseum!");
-            // Define Hero
-            Console.WriteLine(" Please enter the name of your hero: ");
-            heroName = Console.ReadLine();
-            Console.WriteLine("How much health does " + heroName + " have?");
-            heroMaxHealth = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("How much damage does " + heroName + " do per attack?");
-            heroDamage = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("With 1 being very slow, and 10 being very fast, how fast does " + heroName + " attack?");
-            heroAtkSpd = Convert.ToInt32(Console.ReadLine());
+            int dPS = (damage * atkSpd);
 
-            double heroCurrHealth = heroMaxHealth;
-            
-            // Define Enemy
-            Console.WriteLine(" Please enter the name of your enemy: ");
-            enemyName = Console.ReadLine();
-            Console.WriteLine("How much health does " + enemyName + " have?");
-            enemyMaxHealth = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("How much damage does " + enemyName + " do per attack?");
-            enemyDamage = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("With 1 being very slow, and 10 being very fast, how fast does " + enemyName + " attack?");
-            enemyAtkSpd = Convert.ToInt32(Console.ReadLine());
+            Fighter h1 = new Fighter(name, maxHealth, currHealth, damage, atkSpd, dPS, atkRdy, missChance, critRate, dodgeChance);
 
-            double enemyCurrHealth = enemyMaxHealth;
-            
+            Console.WriteLine("Enter the name of your enemy.");
+            string e1name = Console.ReadLine();
+            Console.WriteLine("How much health does " + e1name + " have?");
+            int e1maxHealth = Convert.ToInt32(Console.ReadLine());
+            int e1currHealth = e1maxHealth;
+            Console.WriteLine("How much damage does " + e1name + " do per attack?");
+            int e1damage = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("With 1 being very slow, and 10 being very fast, how fast does " + e1name + " attack?");
+            int e1atkSpd = Convert.ToInt32(Console.ReadLine());
+            int e1DPS = (e1damage + e1atkSpd);
+
+            Fighter e1 = new Fighter(e1name, e1maxHealth, e1currHealth, e1damage, e1atkSpd, e1DPS, atkRdy, missChance, critRate, dodgeChance);
+
             Random rnd = new Random();
 
-            while (heroCurrHealth > 0 && enemyCurrHealth > 0)
+            while (h1.CurrHealth > 0 && e1.CurrHealth > 0)
             {
-                heroAtkRdy += heroAtkSpd;
-                if (heroAtkRdy >= 50)
+                h1.AtkRdy += h1.AtkSpd;
+                if (h1.AtkRdy >= 50)
                 {
-                    heroAtkRdy -= 50;
-                    int heroOutcome = rnd.Next(1, 100);
-                    Console.WriteLine("(" + heroName + " rolled " + heroOutcome + ")");
-                    if (heroOutcome < heroMissChance)
+                    h1.AtkRdy -= 50;
+                    int Outcome = rnd.Next(1, 100);
+                    Console.WriteLine("(" + h1.Name + " rolled " + Outcome + ")");
+                    if (Outcome < h1.MissChance)
                     {
-                        Console.WriteLine(heroName + " misses " + enemyName);
+                        Console.WriteLine(h1.Name + " misses " + e1.Name);
                     }
-                    else if (heroOutcome < heroMissChance + dodgeChance)
+                    else if (Outcome < h1.MissChance + e1.DodgeChance)
                     {
-                        Console.WriteLine(enemyName + " dodged " + heroName + "'s attack!");
+                        Console.WriteLine(e1.Name + " dodged " + h1.Name + "'s attack!");
                     }
-                    else if (heroOutcome < heroMissChance + dodgeChance + critRate)
+                    else if (Outcome < h1.MissChance + e1.DodgeChance + h1.CritRate)
                     {
-                        enemyCurrHealth -= (heroDamage * 2);
-                        Console.WriteLine("A critical strike! " + heroName + " hits " + enemyName + " for " + (heroDamage * 2) + ". " + enemyName + " has " + enemyCurrHealth + " remaining.");
+                        e1.CurrHealth -= (h1.Damage * 2);
+                        Console.WriteLine("A critical strike! " + h1.Name + " hits " + e1.Name + " for " + (h1.Damage * 2) + ". " + e1.Name + " has " + e1.CurrHealth + " remaining.");
                     }
                     else
                     {
-                        enemyCurrHealth -= heroDamage;
-                        Console.WriteLine(heroName + " hits " + enemyName + " for " + heroDamage + ". " + enemyName + " has " + enemyCurrHealth + " remaining.");
+                        e1.CurrHealth -= h1.Damage;
+                        Console.WriteLine(h1.Name + " hits " + e1.Name + " for " + h1.Damage + ". " + e1.Name + " has " + e1.CurrHealth + " remaining.");
                     }
-                    if (enemyCurrHealth <= 0)
+                    if (e1.CurrHealth <= 0)
                     {
-                        double heroHealthRemaining = heroCurrHealth / heroMaxHealth * 100;
-                        Console.WriteLine(heroName + " has defeated " + enemyName + " with " + heroHealthRemaining + "% health remaining.");
+                        double h1HealthRemaining = h1.CurrHealth / h1.MaxHealth * 100;
+                        Console.WriteLine(h1.Name + " has defeated " + e1.Name + " with " + h1HealthRemaining + "% health remaining.");
                         break;
                     }
                 }
-                enemyAtkRdy += enemyAtkSpd;
-                if (enemyAtkRdy >= 50)
+                e1.AtkRdy += e1.AtkSpd;
+                if (e1.AtkRdy >= 50)
                 {
-                    enemyAtkRdy -= 50;
-                    int enemyOutcome = rnd.Next(1, 100);
-                    Console.WriteLine("(" + enemyName + " rolled " + enemyOutcome + ")");
-                    if (enemyOutcome < enemyMissChance)
+                    e1.AtkRdy -= 50;
+                    int Outcome = rnd.Next(1, 100);
+                    Console.WriteLine("(" + e1.Name + " rolled " + Outcome + ")");
+                    if (Outcome < e1.MissChance)
                     {
-                        Console.WriteLine(enemyName + " misses " + heroName);
+                        Console.WriteLine(e1.Name + " misses " + h1.Name);
                     }
-                    else if (enemyOutcome < enemyMissChance + dodgeChance)
+                    else if (Outcome < e1.MissChance + h1.DodgeChance)
                     {
-                        Console.WriteLine(heroName + " dodged " + enemyName + "'s attack!");
+                        Console.WriteLine(h1.Name + " dodged " + e1.Name + "'s attack!");
                     }
-                    else if (enemyOutcome < enemyMissChance + dodgeChance + critRate)
+                    else if (Outcome < e1.MissChance + h1.DodgeChance + e1.CritRate)
                     {
-                        heroCurrHealth -= (enemyDamage * 2);
-                        Console.WriteLine("A critical strike! " + enemyName + " hits " + heroName + " for " + (enemyDamage * 2) + ". " + heroName + " has " + heroCurrHealth + " remaining.");
+                        h1.CurrHealth -= (e1.Damage * 2);
+                        Console.WriteLine("A critical strike! " + e1.Name + " hits " + h1.Name + " for " + (e1.Damage * 2) + ". " + h1.Name + " has " + h1.CurrHealth + " remaining.");
                     }
                     else
                     {
-                        heroCurrHealth -= enemyDamage;
-                        Console.WriteLine(enemyName + " hits " + heroName + " for " + enemyDamage + ". " + heroName + " has " + heroCurrHealth + " remaining.");
+                        h1.CurrHealth -= e1.Damage;
+                        Console.WriteLine(e1.Name + " hits " + h1.Name + " for " + e1.Damage + ". " + h1.Name + " has " + h1.CurrHealth + " remaining.");
                     }
-                    if (heroCurrHealth <= 0)
+                    if (h1.CurrHealth <= 0)
                     {
-                        double enemyHealthRemaining = enemyCurrHealth / enemyMaxHealth * 100;
-                        Console.WriteLine(enemyName + " has defeated " + heroName + " with " + enemyHealthRemaining + "% health remaining.");
+                        double e1HealthRemaining = e1.CurrHealth / e1.MaxHealth * 100;
+                        Console.WriteLine(e1.Name + " has defeated " + h1.Name + " with " + e1HealthRemaining + "% health remaining.");
                         break;
                     }
                 }
